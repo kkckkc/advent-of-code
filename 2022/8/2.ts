50ab52ad599b3d949005e57489ca6f432e0f5b33
Magnus Johansson <magnus.johansson@se.ibm.com>
2022-12-08 08:14:12 +0100

    Minor cleanup

--- 2022/8/2.ts
+++ 2022/8/2.ts
@@ -1,6 +1,7 @@
 import { readFile } from "lib/readFile";
 import { applyPatches } from "lib/patch";
-import { range } from 'lib/arrays';
+import { column, row } from 'lib/arrays';
+import { Grid } from 'lib/grid';
 applyPatches();
 
 type Input = {
@@ -12,24 +13,21 @@ export const parse = (input: string[]): Input => {
 };
 
 export const solve = (input: Input): number => {
-  const w = input.values[0].length;
-  const h = input.values.length;
-
+  const grid = new Grid(input.values);
+  
   const S = (arr: number[], v: number) => {
     const idx = arr.findIndex(c => c >= v);
     return idx === -1 ? arr.length : idx + 1;
   }
 
   let maxScore = 0;
-  for (let x = 1; x < w - 1; x++) {
-    for (let y = 1; y < h - 1; y++) {
-      const beforeRow = range(0, x - 1).map(x2 => input.values[y][x2]).reverse();
-      const afterRow = range(x + 1, w - 1).map(x2 => input.values[y][x2]);
-      const beforeCol = range(0, y - 1).map(y2 => input.values[y2][x]).reverse();
-      const afterCol = range(y + 1, h - 1).map(y2 => input.values[y2][x]);
+  for (let x = 1; x < grid.width() - 1; x++) {
+    for (let y = 1; y < grid.height() - 1; y++) {
+      const [up, down] = grid.column(x).splitAt(y);
+      const [left, right] = grid.row(y).splitAt(x);
 
-      const c = input.values[y][x];
-      maxScore = Math.max(maxScore, S(beforeRow, c) * S(afterRow, c) * S(beforeCol, c) * S(afterCol, c));
+      const c = grid.at(y, x);
+      maxScore = Math.max(maxScore, S(left.reverse(), c) * S(right, c) * S(up.reverse(), c) * S(down, c));
     }
   }
 
--- lib/grid.ts
+++ lib/grid.ts
@@ -1,3 +1,5 @@
+import { range } from './arrays';
+
 export const Grids = {
   adjacent: (includeSelf=false) => [[-1,-1], [-1,0], [-1,1], ...(includeSelf ? [[0, 0]]: []), [0,-1], [0,1], [1,-1], [1,0], [1,1]],
 
@@ -14,3 +16,30 @@ export const Grids = {
   }
 }
 
+export class Grid<T> {
+  data: T[][];
+  
+  constructor(data: T[][]) {
+    this.data = data;
+  }
+
+  column(col: number): T[] {
+    return range(0, this.data.length - 1).map(r => this.data[r][col]);
+  }
+
+  row(row: number): T[] {
+    return this.data[row];
+  }
+
+  at(row: number, column: number) {
+    return this.data[row][column];
+  }
+
+  width() {
+    return this.data[0].length;
+  }
+
+  height() {
+    return this.data.length;
+  }
+}
